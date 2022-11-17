eab4665f5afe3d09a7b79af25ec9a98cda8a2c17
Alejandro Rivera <alejandro.rivera@gmail.com>
2022-11-16 23:24:09 -0500

    Nueva funcion de Selenium.py :D

--- Selenium.py
+++ Selenium.py
@@ -1,9 +1,41 @@
 from selenium import webdriver
-
 from selenium.webdriver.common.by import By
 import time
 
-driver = webdriver.Chrome('C:/Selenium/chromedriver')
-driver.maximize_window()
+from selenium.webdriver.support.ui import WebDriverWait
+
+from contextlib import contextmanager
+
+
+@contextmanager
+def wait_for_new_window(driver, timeout=10):
+    handles_before = driver.window_handles
+    yield
+    WebDriverWait(driver, timeout).until(
+        lambda driver: len(handles_before) != len(driver.window_handles))
+
+
+driver = webdriver.Chrome('C:/chromedriver.exe')
+driver.minimize_window()
 driver.delete_all_cookies()
-driver.get("https://www.amazon.com/")
+driver.get('https://www.amazon.com/')
+
+# Buscar Producto "HP Pavilion azul"
+keyword = "HP Pavilion azul"
+# Crear Elemento web para la barra de busqueda
+search_box = driver.find_element(By.ID, 'twotabsearchtextbox')
+# Colocar la keywoard en la barra de busqueda
+search_box.send_keys(keyword)
+# Crear elemento web para el button de submit
+driver.find_element(By.ID, 'nav-search-submit-button').click()
+
+driver.find_element(
+    By.CSS_SELECTOR, '.a-link-normal').click()
+
+driver.find_element(By.ID, 'quantity').send_keys(2)
+driver.find_element(By.ID, 'add-to-cart-button').click()
+driver.find_element(By.ID, 'sw-gtc').click()
+
+with wait_for_new_window(driver, 2):
+    pass
+driver.quit()
